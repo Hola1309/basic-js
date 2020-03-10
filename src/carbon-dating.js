a129213b9d162f0f767c486b257b42035da35f68
Hola <hola131098@gmail.com>
2020-03-10 23:28:39 +0400

    feat: tasks carbon-dating & dream-team upload

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const K = 0.693 / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity)
+{
+  let num = parseFloat(sampleActivity);
+  if (Number.isNaN(num) || typeof sampleActivity !== 'string') {
+    return false
+  } else if ((num <= 0 || num > 15)) {
+    return false
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY / num)) / K);
 };
